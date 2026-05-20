397df8c9748ef5f654fed76dc86679eedff3c6be
Aurita <sara.a.c.1939@gmail.com>
2026-05-20 11:57:56 -0500

    Implementacion raiz cuadrada

--- JS/funcional.js
+++ JS/funcional.js
@@ -1,3 +1,5 @@
+//crear las propiedades del objeto
+
 let p = {
     teclas: document.querySelectorAll("#calculadora ul li"),
     accion: null,
@@ -8,44 +10,52 @@ let p = {
     resultado: false
 }
 
+//creas los metodos
+
 let m = {
     inicio: function () {
         for (let i = 0; i < p.teclas.length; i++) {
             p.teclas[i].addEventListener("click", m.oprimirtecla);
-        }
-    },
-
-    oprimirtecla: function (tecla) {
+        }         
+    
+    }, oprimirtecla: function (tecla) {
         p.accion = tecla.currentTarget.getAttribute("class");
-        p.digito = tecla.currentTarget.innerHTML;
+        p.digito = tecla.currentTarget.innerHTML; 
         m.calcular(p.accion, p.digito);
-    },
 
-    calcular: function (accion, digito) {
+    }, calcular: function(accion, digito) {
         switch (accion) {
             case "numero":
                 p.cantisimbolo = 0;
                 if (p.operacion.innerHTML == "0") {
                     p.operacion.innerHTML = digito;
                 } else {
-                    if (p.resultado) {
+                    if(p.resultado){
                         p.resultado = false;
                         p.operacion.innerHTML = digito;
                     } else {
                         p.operacion.innerHTML += digito;
                     }
-                }
+                }         
                 break;
 
             case "simbolo":
-                p.cantisimbolo++;
-                if (p.cantisimbolo == 1) {
-                    if (p.operacion.innerHTML == "0") {
-                        p.operacion.innerHTML = "0" + digito;
-                    } else {
-                        p.operacion.innerHTML += digito;
-                        p.cantdecimal = false;
-                        p.resultado = false; 
+                if (digito == "√") {
+                    let valorActual = eval(p.operacion.innerHTML);
+                    p.operacion.innerHTML = Math.sqrt(valorActual);
+                    p.resultado = true;
+                    p.cantisimbolo = 0;
+                    p.cantdecimal = false;
+                } else { 
+                    p.cantisimbolo++;
+                    if (p.cantisimbolo == 1) {
+                        if (p.operacion.innerHTML == "0") {
+                            p.operacion.innerHTML = "0" + digito;
+                        } else {
+                            p.operacion.innerHTML += digito;
+                            p.cantdecimal = false;
+                            p.resultado = false;
+                        }
                     }
                 }
                 break;
@@ -59,21 +69,21 @@ let m = {
                 break;
 
             case "igual":
-            if (p.operacion.innerHTML.includes("/0")) {
-                p.operacion.innerHTML = "Error: Division 0";
-                p.resultado = true;
-            } else {
-                p.operacion.innerHTML = eval(p.operacion.innerHTML);
-                p.resultado = true;
-            }
-            p.cantdecimal = false;
-            p.cantisimbolo = 0;
-            break;
-          
+                if (p.operacion.innerHTML.includes("/0")) {
+                    p.operacion.innerHTML = "Error: Div 0";
+                    p.resultado = true;
+                } else {
+                    p.operacion.innerHTML = eval(p.operacion.innerHTML);
+                    p.resultado = true;
+                }
+                p.cantdecimal = false;
+                p.cantisimbolo = 0;
+                break;
         }
+          
     },
-
-    borrarCalculadora: function () {
+    
+    borrarCalculadora: function() {
         p.operacion.innerHTML = "0";
         p.cantdecimal = false;
         p.cantisimbolo = 0;
@@ -84,10 +94,11 @@ let m = {
 m.inicio();
 document.querySelector("#borrar").addEventListener("click", m.borrarCalculadora);
 
-window.addEventListener("keydown", (e) => {
-    e.preventDefault(); 
+
+window.addEventListener("keydown", function(e) {
     if (e.key >= 0 && e.key <= 9) m.calcular("numero", e.key);
     if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/") m.calcular("simbolo", e.key);
+    if (e.key == "r" || e.key == "R") m.calcular("simbolo", "√"); 
     if (e.key == "." || e.key == ",") m.calcular("decimal", ".");
     if (e.key == "Enter") m.calcular("igual", "=");
     if (e.key == "Escape" || e.key == "Backspace") m.borrarCalculadora();
